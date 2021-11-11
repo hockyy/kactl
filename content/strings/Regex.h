ae8cd96ea139302431784e97d6f1908aaa41753d
hocky <hocky.yudhiono@gmail.com>
2021-11-11 16:31:43 +0700

    Regex

--- content/strings/Regex.h
+++ content/strings/Regex.h
@@ -1,5 +1,5 @@
 /**
- * Author: Asger Hautop Drewsen
+ * Author: Hocky Yudhiono
  * Date: 2017-09-29
  * License: CC0
  * Description: You can use the following special characters: \texttt{\^{}\$.*?|()\{\}}
@@ -7,18 +7,20 @@
  */
 #pragma once
 
-#include <regex.h> /** keep-include */
+#include <regex>
+#include <iostream>
+using namespace std;
 
-struct Regex {
-	regex_t re;
-	Regex(const string & p) {
-		assert(regcomp(&re, p.data(), REG_EXTENDED) == 0);
-	}
-	~Regex() { regfree(&re); }
-	bool search(const string & s, pii & res) {
-		regmatch_t m;
-		if (regexec(&re, s.data(), 1, &m, 0)) return false;
-		res.first = m.rm_so; res.second = m.rm_eo;
-		return true;
-	}
-};
+int main () {
+  string s ("this subject has a submarine as a subsequence");
+  smatch m; regex e ("(sub)([^ ]*)");   // matches words beginning by "sub"
+  // regex search will match the first occurence
+  if(regex_match(begin(s), end(s), regex("this .+"))) cout << "OK" << endl;
+  while (regex_search (s, m, e)) {
+    for (auto x : m) cout << x << " ";
+    cout << endl; s = m.suffix().str();
+  }
+  s = "this subject has a submarine as a subsequence";
+  cout << regex_replace (s,e,"sub-$2");
+  return 0;
+}
