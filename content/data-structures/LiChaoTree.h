47de185a3a829acf182fe00384cd037ef24911cb
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-11-23 20:22:12 +0700

    Add li chao

--- content/data-structures/LiChaoTree.h
+++ content/data-structures/LiChaoTree.h
@@ -4,37 +4,27 @@
  * License: CC0
  * Description: Jago
  */
-
 template<typename data_t>
 struct Line {
   data_t a, b;
-
   Line() : a(0), b(-inf) {}
   Line(data_t a, data_t b) : a(a), b(b) {}
-
   data_t get(data_t x) {
     return a * x + b;
   }
-
   void add(Line x) {
     a += x.a;
     b += x.b;
   }
 };
-
 struct Node {
   Line<data_t> line = Line<data_t>();
   Line<data_t> lazy = Line<data_t>(0, 0);
-
-  Node *lc = nullptr;
-  Node *rc = nullptr;
-
+  Node *lc = nullptr, *rc = nullptr;
   void apply(data_t l, data_t r, Line<data_t> v) {
-    line.add(v);
-    lazy.add(v);
+    line.add(v); lazy.add(v);
   }
 };
-
 void PushLazy(Node* &n, data_t tl, data_t tr) {
   if (n == nullptr) return;
   if (n->lc == nullptr) n->lc = new Node();
@@ -44,7 +34,6 @@ void PushLazy(Node* &n, data_t tl, data_t tr) {
   n->rc->apply(mid + 1, tr, n->lazy);
   n->lazy = Line<data_t>(0, 0);
 }
-
 void PushLine(Node* &n, data_t tl, data_t tr) {
   if (n == nullptr) return;
   data_t mid = (tl + tr) / 2;
@@ -52,7 +41,6 @@ void PushLine(Node* &n, data_t tl, data_t tr) {
   InsertLineKnowingly(n->rc, mid + 1, tr, n->line);
   n->line = Line<data_t>();
 }
-
 void InsertLineKnowingly(Node* &n, data_t tl, data_t tr, Line<data_t> x) {
   if (n == nullptr) n = new Node();
   if (n->line.get(tl) < x.get(tl)) swap(n->line, x);
@@ -67,7 +55,6 @@ void InsertLineKnowingly(Node* &n, data_t tl, data_t tr, Line<data_t> x) {
     InsertLineKnowingly(n->lc, tl, mid, x);
   }
 }
-
 void InsertLine(Node* &n, data_t tl, data_t tr, data_t l, data_t r, Line<data_t> x) {
   if (tr < l || r < tl || tl > tr || l > r) return;
   if (n == nullptr) n = new Node();
@@ -77,40 +64,33 @@ void InsertLine(Node* &n, data_t tl, data_t tr, data_t l, data_t r, Line<data_t>
   InsertLine(n->lc, tl, mid, l, r, x);
   InsertLine(n->rc, mid + 1, tr, l, r, x);
 }
-
 void AddLine(Node* &n, data_t tl, data_t tr, data_t l, data_t r, Line<data_t> x) {
   if (tr < l || r < tl || tl > tr || l > r) return;
   if (n == nullptr) n = new Node();
   if (l <= tl && tr <= r) return n->apply(tl, tr, x);
   data_t mid = (tl + tr) / 2;
-  PushLazy(n, tl, tr);
-  PushLine(n, tl, tr);
+  PushLazy(n, tl, tr); PushLine(n, tl, tr);
   AddLine(n->lc, tl, mid, l, r, x);
   AddLine(n->rc, mid + 1, tr, l, r, x);
 }
-
 data_t Query(Node* &n, data_t tl, data_t tr, data_t x) {
   if (n == nullptr) return -inf;
   if (tl == tr) return n->line.get(x);
   data_t res = n->line.get(x);
   data_t mid = (tl + tr) / 2;
   PushLazy(n, tl, tr);
-  if (x <= mid) {
+  if (x <= mid)
     res = max(res, Query(n->lc, tl, mid, x));
-  } else {
+  else
     res = max(res, Query(n->rc, mid + 1, tr, x));
-  }
   return res;
 }
-
 void InsertLine(data_t l, data_t r, Line<data_t> x) {
   return InsertLine(root, 0, sz - 1, l, r, x);
 }
-
 void AddLine(data_t l, data_t r, Line<data_t> x) {
   return AddLine(root, 0, sz - 1, l, r, x);
 }
-
 data_t Query(data_t x) {
   return Query(root, 0, sz - 1, x);
 }
