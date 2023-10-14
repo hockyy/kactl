dbd03fe83709870f0a61a7654cb4e6900dc18622
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-10-15 02:40:12 +0700

    Shorten aho more

--- content/strings/Aho3S.h
+++ content/strings/Aho3S.h
@@ -15,8 +15,7 @@ struct AhoCorasick {
 	}
 	void add(string str) {
 		int cur = 0;
-		for(int i = 0 ; i < str.length() ; i++) {
-			int nex = str[i] - 'a';
+		rep(i,0,sz(str)) { int nex = str[i] - 'a';
 			if(trie[cur][nex] == -1) {
 				trie[cur][nex] = ++saiz;
 				memset(trie[saiz],-1,sizeof trie[saiz]);
@@ -26,15 +25,11 @@ struct AhoCorasick {
 	}
 	void build() {
 		queue<int> q; fail[0] = 0;
-		rep(i,0,26) {
+		rep(i,0,26)
 			if(trie[0][i] == -1) trie[0][i] = 0;
-			else {
-				int nex = trie[0][i];
-				fail[nex] = 0;
-				q.push(nex);
-			}}
+			else { int nex = trie[0][i]; fail[nex] = 0; q.push(nex); }
 		while(!q.empty()) {
-			int now = q.front();	q.pop();
+			int now = q.front(); q.pop();
 			for(int i = 0 ; i < 26 ; i++)
 				if(trie[now][i] == -1) trie[now][i] = trie[fail[now]][i];
 				else {
@@ -45,10 +40,7 @@ struct AhoCorasick {
 	}
 	int getIndex(string str) {
 		int cur = 0;
-		for(int i = 0 ; i < str.length() ; i++) {
-			//checkChar(str[i]);
-			cur = trie[cur][str[i] - 'a'];
-		}
+		for(const auto &ch : str) cur = trie[cur][ch - 'a'];
 		return cur;
 	}
 };
