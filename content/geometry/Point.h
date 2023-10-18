68cef38cd42fda0bb84ea581767cce261256f92f
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-10-18 13:43:48 +0700

    Add comments

--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -21,7 +21,9 @@ struct Point {
 	P operator-(P p) const { return P(x-p.x, y-p.y); }
 	P operator*(T d) const { return P(x*d, y*d); }
 	P operator/(T d) const { return P(x/d, y/d); }
+	// Same direction means a.b > 0, when perp, a.b = 0
 	T dot(P p) const { return x*p.x + y*p.y; } // a.b = |a|.|b|cos(t)
+	// b left of a, a^b > 0, ccw sort comparator. When perp a.b = 1
 	T cross(P p) const { return x*p.y - y*p.x; } // a^b = |a|.|b|sin(t)
 	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
 	T dist2() const { return x*x + y*y; }
