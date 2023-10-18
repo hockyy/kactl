bc00c26c342a688aaeaa0a546fe81da1f98da92c
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-10-18 12:06:16 +0700

    Shorten sparse 2d

--- content/data-structures/Sparse2D.h
+++ content/data-structures/Sparse2D.h
@@ -10,20 +10,20 @@
  */
 #pragma once
 const int LOGN = 13;
-int sparset[LOGN][LOGN][205][205];
-rep(i,0,n) rep(j,0,m) cin >> sparset[0][0][i][j];
+int st[LOGN][LOGN][205][205];
+rep(i,0,n) rep(j,0,m) cin >> st[0][0][i][j];
 rep(i,0,n) rep(logj,1,LOGN) rep(j,0,m-(1<<logj)+1)
-  sparset[0][logj][i][j] = min(sparset[0][logj-1][i][j],
-  	sparset[0][logj-1][i][j+(1<<(logj-1))]);
+  st[0][logj][i][j] = min(st[0][logj-1][i][j],
+  	st[0][logj-1][i][j+(1<<(logj-1))]);
 rep(logi, 1, LOGN) rep(logj, 0, LOGN)
 	rep(i,0,n-(1<<logi)+1)
 	rep(j,0,m-(1<<logj)+1)
-  	sparset[logi][logj][i][j] = min(sparset[logi-1][logj][i][j],
-  		sparset[logi-1][logj][i+(1<<(logi-1))][j]);
+  	st[logi][logj][i][j] = min(st[logi-1][logj][i][j],
+  		st[logi-1][logj][i+(1<<(logi-1))][j]);
 cin >> st.fi >> st.se >> ed.fi >> ed.se; st.fi--; st.se--;
-int lenrow = 31-__builtin_clz(ed.fi-st.fi);
-int lencol = 31-__builtin_clz(ed.se-st.se);
-res1 = min(min(min(sparset[lenrow][lencol][st.fi][st.se],
-sparset[lenrow][lencol][st.fi][ed.se-(1<<lencol)]),
-sparset[lenrow][lencol][ed.fi-(1<<lenrow)][st.se]),
-sparset[lenrow][lencol][ed.fi-(1<<lenrow)][ed.se-(1<<lencol)]);
+int n = 31-__builtin_clz(ed.fi-st.fi);
+int m = 31-__builtin_clz(ed.se-st.se);
+res1 = min(min(min(st[n][m][st.fi][st.se],
+st[n][m][st.fi][ed.se-(1<<m)]),
+st[n][m][ed.fi-(1<<n)][st.se]),
+st[n][m][ed.fi-(1<<n)][ed.se-(1<<m)]);
