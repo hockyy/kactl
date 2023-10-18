772d55bd1f9ddb914520cd42ddfcbc5c9240bdb2
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-10-18 11:50:17 +0700

    Add li chao tree

--- content/data-structures/LiChaoTree.h
+++ content/data-structures/LiChaoTree.h
@@ -4,12 +4,12 @@
  * License: CC0
  * Description: Jago
  */
-template<typename data_t>
+template<typename T>
 struct Line {
-  data_t a, b;
+  T a, b;
   Line() : a(0), b(-inf) {}
-  Line(data_t a, data_t b) : a(a), b(b) {}
-  data_t get(data_t x) {
+  Line(T a, T b) : a(a), b(b) {}
+  T get(T x) {
     return a * x + b;
   }
   void add(Line x) {
@@ -18,35 +18,35 @@ struct Line {
   }
 };
 struct Node {
-  Line<data_t> line = Line<data_t>();
-  Line<data_t> lazy = Line<data_t>(0, 0);
+  Line<T> line = Line<T>();
+  Line<T> lazy = Line<T>(0, 0);
   Node *lc = nullptr, *rc = nullptr;
-  void apply(data_t l, data_t r, Line<data_t> v) {
+  void apply(T l, T r, Line<T> v) {
     line.add(v); lazy.add(v);
   }
 };
-void PushLazy(Node* &n, data_t tl, data_t tr) {
+void PushLazy(Node* &n, T tl, T tr) {
   if (n == nullptr) return;
   if (n->lc == nullptr) n->lc = new Node();
   if (n->rc == nullptr) n->rc = new Node();
-  data_t mid = (tl + tr) / 2;
+  T mid = (tl + tr) / 2;
   n->lc->apply(tl, mid, n->lazy);
   n->rc->apply(mid + 1, tr, n->lazy);
-  n->lazy = Line<data_t>(0, 0);
+  n->lazy = Line<T>(0, 0);
 }
-void PushLine(Node* &n, data_t tl, data_t tr) {
+void PushLine(Node* &n, T tl, T tr) {
   if (n == nullptr) return;
-  data_t mid = (tl + tr) / 2;
+  T mid = (tl + tr) / 2;
   InsertLineKnowingly(n->lc, tl, mid, n->line);
   InsertLineKnowingly(n->rc, mid + 1, tr, n->line);
-  n->line = Line<data_t>();
+  n->line = Line<T>();
 }
-void InsertLineKnowingly(Node* &n, data_t tl, data_t tr, Line<data_t> x) {
+void InsertLineKnowingly(Node* &n, T tl, T tr, Line<T> x) {
   if (n == nullptr) n = new Node();
   if (n->line.get(tl) < x.get(tl)) swap(n->line, x);
   if (n->line.get(tr) >= x.get(tr)) return;
   if (tl == tr) return;
-  data_t mid = (tl + tr) / 2;
+  T mid = (tl + tr) / 2;
   PushLazy(n, tl, tr);
   if (n->line.get(mid) > x.get(mid)) {
     InsertLineKnowingly(n->rc, mid + 1, tr, x);
@@ -55,29 +55,29 @@ void InsertLineKnowingly(Node* &n, data_t tl, data_t tr, Line<data_t> x) {
     InsertLineKnowingly(n->lc, tl, mid, x);
   }
 }
-void InsertLine(Node* &n, data_t tl, data_t tr, data_t l, data_t r, Line<data_t> x) {
+void InsertLine(Node* &n, T tl, T tr, T l, T r, Line<T> x) {
   if (tr < l || r < tl || tl > tr || l > r) return;
   if (n == nullptr) n = new Node();
   if (l <= tl && tr <= r) return InsertLineKnowingly(n, tl, tr, x);
-  data_t mid = (tl + tr) / 2;
+  T mid = (tl + tr) / 2;
   PushLazy(n, tl, tr);
   InsertLine(n->lc, tl, mid, l, r, x);
   InsertLine(n->rc, mid + 1, tr, l, r, x);
 }
-void AddLine(Node* &n, data_t tl, data_t tr, data_t l, data_t r, Line<data_t> x) {
+void AddLine(Node* &n, T tl, T tr, T l, T r, Line<T> x) {
   if (tr < l || r < tl || tl > tr || l > r) return;
   if (n == nullptr) n = new Node();
   if (l <= tl && tr <= r) return n->apply(tl, tr, x);
-  data_t mid = (tl + tr) / 2;
+  T mid = (tl + tr) / 2;
   PushLazy(n, tl, tr); PushLine(n, tl, tr);
   AddLine(n->lc, tl, mid, l, r, x);
   AddLine(n->rc, mid + 1, tr, l, r, x);
 }
-data_t Query(Node* &n, data_t tl, data_t tr, data_t x) {
+T Query(Node* &n, T tl, T tr, T x) {
   if (n == nullptr) return -inf;
   if (tl == tr) return n->line.get(x);
-  data_t res = n->line.get(x);
-  data_t mid = (tl + tr) / 2;
+  T res = n->line.get(x);
+  T mid = (tl + tr) / 2;
   PushLazy(n, tl, tr);
   if (x <= mid)
     res = max(res, Query(n->lc, tl, mid, x));
@@ -85,12 +85,12 @@ data_t Query(Node* &n, data_t tl, data_t tr, data_t x) {
     res = max(res, Query(n->rc, mid + 1, tr, x));
   return res;
 }
-void InsertLine(data_t l, data_t r, Line<data_t> x) {
+void InsertLine(T l, T r, Line<T> x) {
   return InsertLine(root, 0, sz - 1, l, r, x);
 }
-void AddLine(data_t l, data_t r, Line<data_t> x) {
+void AddLine(T l, T r, Line<T> x) {
   return AddLine(root, 0, sz - 1, l, r, x);
 }
-data_t Query(data_t x) {
+T Query(T x) {
   return Query(root, 0, sz - 1, x);
 }
