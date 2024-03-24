d4f402103280752d80492ecc731108c902174ec5
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2024-03-24 21:33:53 +0800

    Update ClockTime.h

--- content/various/ClockTime.h
+++ content/various/ClockTime.h
@@ -11,4 +11,8 @@ clock_t first_attempt = clock();
 inline void cek_time(){
 	clock_t cur = clock()- first_attempt;
 	cerr<<"TIME : "<<(double) cur/CLOCKS_PER_SEC<<endl;
+      auto current = chrono::high_resolution_clock::now();
+      chrono::duration<double> elapsed = current - start;
+      // cout << fixed << setprecision(3) << elapsed.count() << "\n";
+      if (elapsed.count() < runtime[i].se) continue;
 }
